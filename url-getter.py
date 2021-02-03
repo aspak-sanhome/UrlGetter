267799901079f18695312c4b0f1c6d7bcfc32a51
Tony <aspak@sanhome.eu>
2021-02-03 12:49:08 +0100

    update

--- url-getter.py
+++ url-getter.py
@@ -24,13 +24,17 @@ import urllib.request
 import time
 import random
 
-f = open("urls.txt", "r")
+timesleep = 1
 user_agent = ['Mozilla/5.0 (Windows NT 6.1; Win64; x64)', 'Mozilla/5.0 (X11; Linux x86_64)', 'Mozilla/5.0 (X11; U; Linux i686; en-US)','Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3)', 'Opera/9.80 (X11; Linux i686; Ubuntu/14.10)', 'Opera/9.80 (Windows NT 6.0)', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)']
-for x in f:
-  print("call: ", x) 
-  req = urllib.request.Request(x, headers={'User-Agent': random.choice(user_agent)})
-  try: urllib.request.urlopen(req, timeout=10)
-  except urllib.error.URLError as e:
-      print(e.reason)
-  time.sleep(5)
-
+while True:
+    f = open("urls.txt", "r")
+    for x in f:
+      print("call: ", x) 
+      req = urllib.request.Request(x, headers={'User-Agent': random.choice(user_agent)})
+      try: urllib.request.urlopen(req, timeout=10)
+      except urllib.error.URLError as e:
+          print(e.reason)
+      time.sleep(5)
+    print("Uahh tired ...going to sleep for ", timesleep, " hour(s)")
+    time.sleep(timesleep*3600)
+    
