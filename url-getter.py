be984a74d3237e39ab2d70da55d0e36eeadba913
Ubuntu <AzureUser@VM-TrafficGen.fbtpn42hkykuvco3oott30d2gg.bx.internal.cloudapp.net>
2021-11-25 17:25:34 +0000

    ignore SSL Interception

--- url-getter.py
+++ url-getter.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 """  
                                                                       ./*       
                                                                  .(###.         
@@ -23,15 +25,20 @@
 import urllib.request
 import time
 import random
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 timesleep = 1
 user_agent = ['Mozilla/5.0 (Windows NT 6.1; Win64; x64)', 'Mozilla/5.0 (X11; Linux x86_64)', 'Mozilla/5.0 (X11; U; Linux i686; en-US)','Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3)', 'Opera/9.80 (X11; Linux i686; Ubuntu/14.10)', 'Opera/9.80 (Windows NT 6.0)', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)']
 while True:
-    f = open("urls.txt", "r")
+    f = open("url.txt", "r")
     for x in f:
       print("call: ", x) 
       req = urllib.request.Request(x, headers={'User-Agent': random.choice(user_agent)})
-      try: urllib.request.urlopen(req, timeout=10)
+      try: urllib.request.urlopen(req, context=ctx, timeout=10)
       except urllib.error.URLError as e:
           print(e.reason)
           pass
